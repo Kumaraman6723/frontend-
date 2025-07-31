3edb556896a96c6b3dea49981ceab4e5238c1d37
Aman kumar prasad <kumarprasadaman1234@gmail.com>
2025-07-31 13:26:52 +0530

    Refactor Vercel and Vite configurations; remove loading spinner and error boundary components from the application

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,13 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store.js";
-import ErrorBoundary from "./components/ErrorBoundary.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ErrorBoundary>
-      <Provider store={store}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </Provider>
-    </ErrorBoundary>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
